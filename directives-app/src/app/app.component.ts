f1f989c9f82e3a3f5a30bd1e74fdb06b1ebb8cc0
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-10-09 20:56:56 +0530

    clazz sample

--- directives-app/src/app/app.component.ts
+++ directives-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StyleComponent } from "./style/style.component";
+import { ClazzComponent } from "./clazz/clazz.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StyleComponent],
+  imports: [RouterOutlet, StyleComponent, ClazzComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- directives-app/src/app/clazz/clazz.component.ts
+++ directives-app/src/app/clazz/clazz.component.ts
@@ -1,23 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-clazz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Add CommonModule here
   templateUrl: './clazz.component.html',
-  styleUrl: './clazz.component.css'
+  styleUrls: ['./clazz.component.css']
 })
 export class ClazzComponent {
-  marks: number=70;
-  myclass: string="";
-  constructor(){
-    if(this.marks>=70)
-    {
-      this.myclass="class1";
-    }
-    else{
-      this.myclass="class2";
-    }
+  marks: number = 70;
+  myclass: string = "";
+  constructor() {
+    this.myclass = this.marks >= 70 ? "class1" : "class2";
   }
-
 }
