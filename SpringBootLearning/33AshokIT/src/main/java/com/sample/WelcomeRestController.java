74306faf105a0548da7d9f961c32a7eecb0f7f86
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-10-13 20:03:37 +0530

    validate

---
+++ SpringBootLearning/33AshokIT/src/main/java/com/sample/WelcomeRestController.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class WelcomeRestController {
+	
+	@GetMapping("/welcome")
+	public String getWelcomeMsg() {
+		return "Welcome to Ashok IT";
+		
+	}
+
+}
