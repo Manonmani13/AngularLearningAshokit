defcccd30a1ba7c7cb32e8d3075b6efe51a93b08
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-10-14 20:55:37 +0530

    hellow orld example

---
+++ login-app/src/app/hello-world.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+@Component({
+    selector:'app-hello-world',
+    templateUrl:'./hello-word.component.html',
+    styleUrl:'./hello-world.component.css'
+})
+class HelloWorldComponent{
+    title="Hello world";
+}
