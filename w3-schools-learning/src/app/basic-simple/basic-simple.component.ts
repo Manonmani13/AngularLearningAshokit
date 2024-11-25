6bdd263559dfce1fb583f18bcc5b67a802545e25
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-11-25 20:14:25 +0530

    angular 1

---
+++ w3-schools-learning/src/app/basic-simple/basic-simple.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-simple',
+  standalone: true,
+  imports: [],
+  templateUrl: './basic-simple.component.html',
+  styleUrl: './basic-simple.component.css'
+})
+export class BasicSimpleComponent {
+
+}
