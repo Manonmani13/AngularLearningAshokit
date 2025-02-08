e2bcb9e7b24a093bf3d22dfb5beee8086545f02d
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-02-08 17:16:59 +0530

    typescript

---
+++ TypeScript/dataTypes.ts
@@ -0,0 +1,14 @@
+let a:number;
+let b:string;
+let c:boolean;
+let d:any;
+let e:number[]=[1,2,3];
+let f:any[]=[1,'a',true];
+
+const colorRed=0;
+const colorBlue=1;
+const colorGreen=2;
+
+enum Color { Red=101, Blue=102, Green=103};
+
+let backgroundColor=Color.Blue;
---
+++ TypeScript/declarevariable.ts
@@ -0,0 +1,9 @@
+var number=1;
+let count=3;
+
+function doSomething(){
+    for(var i=0;i<5;i++){
+        console.log(i);
+    }
+    console.log("finaly ",i)
+}
