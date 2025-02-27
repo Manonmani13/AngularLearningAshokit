5281249c7d8b667347f4f7fcd3ebba80218ffd98
Manonmani13 <manonmani.pandukumar@gmail.com>
2025-02-27 20:31:13 +0530

    angular

--- LearningAngularBasics/hello-world/src/app/template-driven-form/template-driven-form.component.ts
+++ LearningAngularBasics/hello-world/src/app/template-driven-form/template-driven-form.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-form',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './template-driven-form.component.html',
   styleUrl: './template-driven-form.component.css'
 })
-export class TemplateDrivenFormComponent {
+export class TemplateDrivenFormComponent  implements OnInit{
     countryList: Country[]=[
-      new Country('1','India'),
-      new Country('2','Madurai'),
-    ]
+      new Country('1','USA'),
+      new Country('2','UK'),
+      new Country('3','Canada'),
+      new Country('4','Australia'),
+
+    ];
+    contact!:Contact;
+
+    onSubmit(form:NgForm){
+      console.log(form.value);
+    }
+    ngOnInit(): void {
+        this.contact={
+           firstName:"manonmani",
+           lastName:"pandukumar",
+           email:"manonmani.pandukumar@gmail.com",
+           gender:"female",
+           isMarried:false,
+           country:"2",
+           address:{city:"chennai",street:"no 24,plot no:15 vallal paari nagar 2nd main road",pincode:"400050"}
+
+        }
+    }
 }
 class Country{
   id:string;
@@ -19,4 +41,18 @@ class Country{
     this.id=id;
     this.name=name;
   }
+}
+class Contact{
+  firstName!:string;
+  lastName!:string;
+  email!: string;
+  gender!: string;
+  isMarried!:boolean;
+  country!:string;
+  address!:{
+    city:string;
+    street:string;
+    pincode:string;
+  }
+
 }
